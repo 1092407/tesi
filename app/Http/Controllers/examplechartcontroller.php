3918acc721d4953781839bea3120c29df35edd05
giorg <s1092407@studenti.univpm.it>
2022-12-23 16:22:52 +0100

    sistemato questione dei chart per valori interi

--- app/Http/Controllers/examplechartcontroller.php
+++ app/Http/Controllers/examplechartcontroller.php
@@ -5,6 +5,7 @@ use App\Charts\examplechart;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Resources\Auto; //la uso per fare qualche prova sui chart
 
 use Illuminate\Support\Facades\Log;
 
@@ -15,8 +16,24 @@ class examplechartController extends Controller
 
      public function index(){
         $chart = new examplechart;
-        $chart->labels(['', '', '','']);
-        $chart->dataset('My datase', 'line', [1, 2, 3,4,8]);
+
+        $allid=Auto::select("id")->get()->toArray();
+        $valori=[];
+
+        for ($i=0;$i<count($allid);$i++){
+        $app=Auto::where("id",$allid[$i])->value("id");
+        $valori[$i]=$app;
+        }
+
+        $lab=[];
+        for ($i=0;$i<count($allid);$i++){
+        $lab[$i]="";
+        }
+
+
+
+        $chart->labels(array_values($lab));
+        $chart->dataset('dati ', 'line', array_values($valori));  //così funziona
 
            return view('sample_view', compact('chart'));
 
